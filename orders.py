57ee9ce368707469cf32f6b59abed1fd02e492bf
Sam Sheehy <samuelsheehy95@gmail.com>
2023-01-25 09:32:17 +0000

    Implement orders management endpoints

--- orders.py
+++ orders.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, request
 from db import get_connection
 
 orders_pages = Blueprint('orders', __name__, url_prefix='/orders')
@@ -7,29 +7,47 @@ orders_pages = Blueprint('orders', __name__, url_prefix='/orders')
 @orders_pages.route('/', methods=['GET'])
 def list_orders():
     conn = get_connection()
-    cursor = conn.cursor()
-    orders_list = cursor.execute("SELECT * FROM orders").fetchall()
+    orders_list = conn.execute("SELECT * FROM orders").fetchall()
     return jsonify(orders_list)
 
 
 @orders_pages.route('/<order_id>', methods=['GET'])
 def get(order_id):
-    pass
+    order = get_connection().execute("SELECT * FROM orders WHERE id=?", (order_id)).fetchall()
+    if len(order) == 0:
+        return abort(404)
+    elif len(order) > 1:
+        # this condition should never happen as long as there is
+        # a uniqueness constraint on id
+        return abort(500)
+    else:
+        return jsonify(order[0])
 
 
 @orders_pages.route('/<order_id>', methods=['DELETE'])
 def delete(order_id):
-    pass
+    get_connection().execute("DELETE FROM orders WHERE id=?", (order_id))
+    return jsonify(success=True)
 
 
 @orders_pages.route('/<order_id>', methods=['PUT'])
 def update(order_id):
-    pass
+    actual_price = request.get_json()["actual_price"]
+    get_connection().execute(
+        "UPDATE orders SET actual_price=? WHERE id=?",
+        (actual_price, order_id)
+    )
+    return jsonify(success=True)
 
 
 @orders_pages.route('/', methods=['POST'])
 def post():
-    pass
+    data = request.get_json()
+    get_connection().execute(
+        "INSERT INTO orders (id, product_id, actual_price) VALUES (?, ?, ?)",
+        (data["id"], data["product_id"], data["actual_price"])
+    )
+    return jsonify(success=True)
 
 
 @orders_pages.route('/metrics', methods=['GET'])
--- tests/test_api.py
+++ tests/test_api.py
@@ -2,19 +2,70 @@ import pytest
 import json
 from main import app
 
+
+JSON_HEADER = {'content-type': 'application/json'}
+
+
 @pytest.fixture()
 def client():
     return app.test_client()
 
 
+def create_order_entry(db_conn):
+    db_conn.execute("INSERT INTO products (name, list_price) VALUES (?, ?)", ('Catamaran', 1000))
+    db_conn.execute("INSERT INTO orders (id, product_id, actual_price) VALUES (?, ?, ?)", (1, 1, 90))
+
 
 def test_orders_list(client, db_conn):
-    cur = db_conn.cursor()
-    cur.execute("INSERT INTO products (name, list_price) VALUES (?, ?)", ('Catamaran', 1000))
-    cur.execute("INSERT INTO orders (id, product_id, actual_price) VALUES (?, ?, ?)", (1, 1, 90))
-    db_conn.commit()
+    create_order_entry(db_conn)
     response = client.get("/orders/")
     assert response.status_code == 200
     data = json.loads(response.data)
     assert isinstance(data, list)
     assert len(data) == 1
+
+
+def test_orders_get_404(client, db_conn):
+    response = client.get("/orders/1")
+    assert response.status_code == 404
+
+
+def test_orders_get_success(client, db_conn):
+    create_order_entry(db_conn)
+    response = client.get("/orders/1")
+    assert response.status_code == 200
+    assert json.loads(response.data) == dict(id=1, product_id=1, actual_price=90)
+
+
+def test_orders_delete(client, db_conn):
+    create_order_entry(db_conn)
+    response = client.delete("/orders/1")
+    assert response.status_code == 200
+    order = db_conn.execute("SELECT * FROM orders WHERE id=1").fetchone()
+    assert order is None
+
+
+def test_orders_update(client, db_conn):
+    create_order_entry(db_conn)
+
+    response = client.put(
+        "/orders/1",
+        data=json.dumps({"actual_price": 5}),
+        headers=JSON_HEADER
+    )
+    assert response.status_code == 200
+    order = db_conn.execute("SELECT * FROM orders WHERE id=1").fetchone()
+    assert order["actual_price"] == 5
+
+
+def test_orders_post_success(client, db_conn):
+    db_conn.execute("INSERT INTO products (name, list_price) VALUES (?, ?)", ('Catamaran', 1000))
+    payload = dict(
+        id=1,
+        product_id=1,
+        actual_price=75
+    )
+    response = client.post("/orders/", data=json.dumps(payload), headers=JSON_HEADER)
+    assert response.status_code == 200
+    order = db_conn.execute("SELECT * FROM orders WHERE id=1").fetchone()
+    assert order == payload
