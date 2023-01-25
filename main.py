4902ded2f5ed0199c6f4cdca8e9003ce1a82842d
Sam Sheehy <samuelsheehy95@gmail.com>
2023-01-25 12:17:35 +0000

    Fix commit bug to database

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ app = Flask(__name__)
 app.register_blueprint(orders_pages)
 app.config['DATABASE'] = "db.sqlite3"
 
+@app.route("/")
+def home():
+    with open("app.html") as f:
+        return f.read()
+
 
 # close database connection
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.commit()
         g.sqlite_db.close()
 
 
 if __name__ == "__main__":
+    app.config["DEBUG"] = True
     app.run()
